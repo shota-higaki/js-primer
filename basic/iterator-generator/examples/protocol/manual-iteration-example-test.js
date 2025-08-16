776348ca59ba45747f3ab61c76ed052f731bfedf
shota-higaki <shota-higaki@users.noreply.github.com>
2025-08-16 10:10:42 +0000

    deploy: b9884da4502036da6eb8b967d30af07a5de9eea7

--- basic/iterator-generator/examples/protocol/manual-iteration-example-test.js
+++ basic/iterator-generator/examples/protocol/manual-iteration-example-test.js
@@ -1,28 +1,25 @@
 import assert from "assert";
 import strictEval from "strict-eval";
 import fs from "fs";
+import path from "path";
 import url from "node:url";
-import path from "node:path";
 import { describe, it } from "node:test";
 
 const __filename__ = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename__);
-const Code = fs.readFileSync(path.join(__dirname, "../../src/for-in/for-in-object-example.js"), "utf-8");
-describe("for-in-object", function() {
-    it("オブジェクトのkey:valueが列挙される", function() {
+const Code = fs.readFileSync(path.join(__dirname, "./manual-iteration.example.js"), "utf-8");
+
+describe("manual-iteration", function() {
+    it("手動でIteratorを操作して値を取得できる", function() {
         const actualLogs = [];
-        const console = {
+        const consoleMock = {
             log(message) {
                 actualLogs.push(message);
             }
         };
         strictEval(Code, {
-            console
+            console: consoleMock
         });
-        assert.deepEqual(actualLogs, [
-            "key:a, value:1",
-            "key:b, value:2",
-            "key:c, value:3",
-        ]);
+        assert.deepStrictEqual(actualLogs, [1, 2, 3]);
     });
 });
